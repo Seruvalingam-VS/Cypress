1827cc15a57fd0d18cb9620800135af11431962a
Seruvalingam-VS <158252823+Seruvalingam-VS@users.noreply.github.com>
2024-02-12 17:32:39 +0530

    testing

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,7 @@
+/// <reference types = "Cypress"/>
+
+describe("test case",()=>{
+    it("test case",()=>{
+        cy.visit("https://naveenautomationlabs.com/opencart/index.php?route=account/register")
+    })
+})
