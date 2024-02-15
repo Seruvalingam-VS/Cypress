f7fe239e96514732634c247ea15d9a2e146f278b
Seruvalingam-VS <158252823+Seruvalingam-VS@users.noreply.github.com>
2024-02-15 12:06:49 +0530

    Html Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity :false,
+  video:true,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
